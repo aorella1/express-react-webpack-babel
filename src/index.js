bf9dbe0071110a6a3e5d7198347257d9f3039bec
aorella1 <aorella1@binghamton.edu>
2020-06-06 16:18:52 -0400

    remove test file imports and test string

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './css/styles.css';
-
 ReactDOM.render(
   <>
-    <p>Hello friends there from react</p>
   </>,
   document.getElementById('root')
 )
