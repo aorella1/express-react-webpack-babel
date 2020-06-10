643988e5160fb76629a71c32b83f648e8e2736fc
aorella1 <aorella1@binghamton.edu>
2020-06-10 14:07:20 -0400

    Add support for ssr lazy loading and automatic chunk naming

--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,10 @@ module.exports = {
               presets: [
                 "@babel/preset-env",
                 "@babel/preset-react"],
-              plugins: ['@babel/plugin-syntax-dynamic-import']
+              plugins: [
+                '@babel/plugin-syntax-dynamic-import',
+                '@loadable/babel-plugin'
+              ]
             }
           }
         ]
