1f2d43cdc8c6426cfba623a104fe789768fb8a86
aorella1 <aorella1@binghamton.edu>
2020-11-23 15:23:48 -0500

    Add boilerplate api call

--- server/server.js
+++ server/server.js
@@ -1,3 +1,20 @@
-import express from 'express';
+const express = require('express');
+const path = require('path');
+const app = express()
+const axios = require("axios");
 
-console.log('hello from server');
+app.use(express.static(path.join(__dirname, '../', 'build')));
+app.use(express.json())
+
+app.get('/getAPIResponse', (req, res, next) => {
+  console.log("'/test' call");
+  axios.get("https://jsonplaceholder.typicode.com/todos/1")
+    .then(response => (response.data))
+    .catch(err => next(err));
+})
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
+});
+
+app.listen(9000);
